1a45eb55c369877eb68c25d3becb7bc923032758
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-11-24 19:43:42 -0800

    Update the Register route in the fron-end

--- express-back-end/database/seeds/03_invitations.sql
+++ express-back-end/database/seeds/03_invitations.sql
@@ -2,13 +2,16 @@ INSERT INTO invitations (guest_id, cooking_session_id, status) VALUES
 (1, 1, 'Accepted'),
 (2, 1, 'Accepted'),
 (3, 1, 'Accepted'),
+(4, 1, 'Accepted'),
 
 (2, 2, 'Accepted'),
 (1, 2, 'Pending'),
 (3, 2, 'Declined'),
+(4, 2, 'Declined'),
 
 (3, 3, 'Accepted'),
 (1, 3, 'Pending'),
+(4, 3, 'Pending'),
 
 (4, 4, 'Accepted'),
 (1, 4, 'Declined'),
