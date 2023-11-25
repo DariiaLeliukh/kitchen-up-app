77985fd4d01770d6b3b9a58dab82908a90447d79
Rauber Simoes do Nascimento <raubersn@gmail.com>
2023-11-25 15:06:04 -0800

    Implement the back-end for Recipe Info

--- express-back-end/database/queries/cooking-sessions.js
+++ express-back-end/database/queries/cooking-sessions.js
@@ -13,11 +13,15 @@ const getCookingSessions = (guestId) => {
   });
 };
 
-const getCookingSession = (id) => {
-  return db.query(`SELECT cooking_sessions.*, users.first_name || ' ' || users.last_name AS host_name
+const getCookingSession = (cookingSessionId, guestId) => {
+  return db.query(`SELECT cooking_sessions.*, 
+                    users.first_name || ' ' || users.last_name AS host_name,
+                    (host_id = guest_id) as is_host
                   FROM cooking_sessions
                   JOIN users ON cooking_sessions.host_id = users.id
-                  WHERE cooking_sessions.id = $1`,[id]).then((data) => {
+                  JOIN invitations ON cooking_sessions.id = invitations.cooking_session_id
+                  WHERE cooking_sessions.id = $1
+                  AND guest_id = $2`, [cookingSessionId, guestId]).then((data) => {
     return data.rows;
   });
 };
--- express-back-end/routes/cooking-sessions.js
+++ express-back-end/routes/cooking-sessions.js
@@ -8,8 +8,8 @@
 const express = require("express");
 const router = express.Router();
 // const { Server } = require("socket.io");
-// const axios = require('axios');
-// const recipeApiUrl = require('../routes/helper/api-routes');
+const axios = require('axios');
+const recipeApiUrl = require('../routes/helper/api-routes');
 const sessionsQuery = require('../database/queries/cooking-sessions');
 const usersQuery = require('../database/queries/users');
 
@@ -27,20 +27,22 @@ router.get("/", async (req, res) => {
   });
 });
 
-// router.get("/:id/info", async (req, res) => {
-//   const access_token = req.cookies.jwt;
+router.get("/:id/info", async (req, res) => {
+  const cookingSessionId = req.params.id;
+  const access_token = req.cookies.jwt;
+  const foundUser = await usersQuery.getUserByToken(access_token);
 
-//   //Get the cooking session details
-//   const cookingSession = await usersQuery.getUserByToken(access_token);
-
-//   sessionsQuery.getCookingSession(cookingSession.id).then(async (cooking_session) => {
-//     //get the recipe's summary
-//     await axios.get(recipeApiUrl.getRecipeSummary(cooking_session.id));
-    
-//     //joing the data
-//     cooking_session_info = {...cookingSession
-//     res.json(cooking_session_info);
-//   });
-// });
+  //get the cooking session info
+  sessionsQuery.getCookingSession(cookingSessionId, foundUser.id).then((cookingSession) => {
+    //get the recipe details
+    axios.get(recipeApiUrl.getRecipeSummary(cookingSession[0].api_recipe_id)).then((recipe) => {
+      //join the data & send the response
+      res.json({ ...cookingSession[0], summary: recipe.data.summary });
+    });
+  })
+  .catch(error => {
+    console.error('At least one connection was rejected:', error);
+  });
+});
 
 module.exports = router;
