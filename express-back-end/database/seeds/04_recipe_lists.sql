a28119bf45a41ca686b233e0a52e9290f1685ea1
DariiaLeliukh <dariialelyukh@gmail.com>
2023-12-06 19:42:02 -0600

    A bit more seeds

--- express-back-end/database/seeds/04_recipe_lists.sql
+++ express-back-end/database/seeds/04_recipe_lists.sql
@@ -1,4 +1,5 @@
 INSERT INTO recipe_lists (user_id, name, created_at, modified_at) VALUES
 (1, 'My list', '2023-01-15', '2023-01-15'),
 (3, 'Fridaynight Dinners', '2023-12-01', '2023-12-01'),
-(1, 'X-mas', '2023-12-25', '2023-12-01');
+(1, 'X-mas', '2023-12-25', '2023-12-01'),
+(2, 'Desserts', '2023-01-15', '2023-01-15');
--- express-back-end/database/seeds/06_recipe_list_items.sql
+++ express-back-end/database/seeds/06_recipe_list_items.sql
@@ -2,4 +2,7 @@ INSERT INTO recipe_list_items (recipe_list_id, api_recipe_id) VALUES
 (3, 655455),
 (3, 654274),
 (2, 632353),
-(1, 451248);
+(1, 451248),
+(4, 645530),
+(4, 639116),
+(4, 716367);
